aec66c8c686661546e9c402dde38bbdfb2b88a23
saveole <17688703451@163.com>
2024-09-08 17:24:43 +0800

    other test cases

--- part1-election/go.mod
+++ part1-election/go.mod
@@ -1,3 +1,5 @@
 module raft-go/part1-election
 
 go 1.23.1
+
+require github.com/fortytw2/leaktest v1.3.0 // indirect
--- part1-election/raft_test.go
+++ part1-election/raft_test.go
@@ -1,6 +1,11 @@
 package raft
 
-import "testing"
+import (
+	"testing"
+	"time"
+
+	"github.com/fortytw2/leaktest"
+)
 
 func TestElectionBasic(t *testing.T) {
 	h := NewHarness(t, 3)
@@ -24,5 +29,138 @@ func TestElectionLeaderDisconnect(t *testing.T) {
 	}
 	if newTerm <= origTerm {
 		t.Errorf("want new term to be greater than orig term")
+	}
+}
+
+func TestElectionLeaderAndAnotherDisconnect(t *testing.T) {
+	h := NewHarness(t, 3)
+	defer h.Shutdown()
+
+	origLeaderId, _ := h.CheckSingleLeader()
+	h.DisconnectPeer(origLeaderId)
+
+	otherId := (origLeaderId + 1) / 3
+	h.DisconnectPeer(otherId)
+
+	// No quorum
+	sleepMs(450)
+	h.CheckNoLeader()
+
+	// Reconnect one other server; now we have quorum
+	h.ReconnectPeer(otherId)
+	h.CheckSingleLeader()
+}
+
+func TestDisconnectAllThenRestore(t *testing.T) {
+	h := NewHarness(t, 3)
+	defer h.Shutdown()
+
+	sleepMs(100)
+
+	// Disconnect all servers, there will be no leader.
+	for i := 0; i < 3; i++ {
+		h.DisconnectPeer(i)
+	}
+	sleepMs(450)
+	h.CheckNoLeader()
+
+	// Reconnect all servers, there should be a leader.
+	for i := 0; i < 3; i++ {
+		h.ReconnectPeer(i)
+	}
+	h.CheckSingleLeader()
+}
+
+func TestElectionLeaderDisconnectThenReconnect(t *testing.T) {
+	h := NewHarness(t, 3)
+	defer h.Shutdown()
+
+	origLeaderId, _ := h.CheckSingleLeader()
+
+	h.DisconnectPeer(origLeaderId)
+
+	sleepMs(350)
+	newLeaderId, newTerm := h.CheckSingleLeader()
+
+	h.ReconnectPeer(origLeaderId)
+	sleepMs(150)
+
+	againLeaderId, againTerm := h.CheckSingleLeader()
+
+	if newLeaderId != againLeaderId {
+		t.Errorf("again leader id got %d; want %d", againLeaderId, newLeaderId)
+	}
+	if againTerm != newTerm {
+		t.Errorf("again term got %d; want %d", againTerm, newTerm)
+	}
+}
+
+func TestElectionLeaderDisconnectThenReconnect5(t *testing.T) {
+	defer leaktest.CheckTimeout(t, 100*time.Millisecond)()
+
+	h := NewHarness(t, 5)
+	defer h.Shutdown()
+
+	origLeaderId, _ := h.CheckSingleLeader()
+	h.DisconnectPeer(origLeaderId)
+	sleepMs(150)
+
+	newLeaderId, newTerm := h.CheckSingleLeader()
+	h.ReconnectPeer(origLeaderId)
+	sleepMs(150)
+
+	againLeaderId, againTerm := h.CheckSingleLeader()
+
+	if newLeaderId != againLeaderId {
+		t.Errorf("again leader id got %d; want %d", againLeaderId, newLeaderId)
+	}
+	if againTerm != newTerm {
+		t.Errorf("again term got %d; want %d", againTerm, newTerm)
+	}
+}
+
+func TestElectionFollowerComesBack(t *testing.T) {
+	defer leaktest.CheckTimeout(t, 100*time.Millisecond)()
+
+	h := NewHarness(t, 3)
+	defer h.Shutdown()
+
+	origLeaderId, origTerm := h.CheckSingleLeader()
+
+	otherId := (origLeaderId + 1) / 3
+	h.DisconnectPeer(otherId)
+	time.Sleep(650 * time.Millisecond)
+	h.ReconnectPeer(otherId)
+	sleepMs(150)
+
+	// We can't have an assertion on the new leader id here because it depends
+	// on the relative election timeouts. We can assert that the term changed,
+	// however, which implies that re-election has occurred.
+	_, newTerm := h.CheckSingleLeader()
+	if newTerm <= origTerm {
+		t.Errorf("newTerm=%d, origTerm=%d", newTerm, origTerm)
+	}
+}
+
+func TestElectionDisconnectLoop(t *testing.T) {
+	defer leaktest.CheckTimeout(t, 100*time.Millisecond)()
+
+	h := NewHarness(t, 3)
+	defer h.Shutdown()
+
+	for cycle := 0; cycle < 5; cycle++ {
+		leaderId, _ := h.CheckSingleLeader()
+		h.DisconnectPeer(leaderId)
+		otherId := (leaderId + 1) / 3
+		h.DisconnectPeer(otherId)
+		sleepMs(310)
+		h.CheckNoLeader()
+
+		// Reconnect both
+		h.ReconnectPeer(otherId)
+		h.ReconnectPeer(leaderId)
+
+		// Wait for leader to be elected
+		sleepMs(150)
 	}
 }
